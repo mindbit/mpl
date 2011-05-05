bb355f35c1550d3d0feac3040237446945423b64
Radu Rendec <radu.rendec@mindbit.ro>
2011-05-05 11:37:59 +0300

    Add thin wrapper over DOMDocument
    For now, this is just to throw exceptions (instead of generating errors)
    from the load() method.
    
    Signed-off-by: Radu Rendec <radu.rendec@mindbit.ro>

---
+++ MplDOMDocument.php
@@ -0,0 +1,20 @@
+<?
+require_once "ThrowErrorHandler.php";
+
+class MplDOMDocument extends DOMDocument {
+	function load($filename, $options = 0) {
+		$exception = null;
+		$prevErrorHandler = ErrorHandler::setHandler(new ThrowErrorHandler());
+		try {
+			$ret = parent::load($filename, $options);
+		} catch (Exception $__e) {
+			$exception = $__e;
+		}
+		ErrorHandler::setHandler($prevErrorHandler);
+		if ($exception !== null)
+			throw $exception;
+		return $ret;
+	}
+}
+
+?>
