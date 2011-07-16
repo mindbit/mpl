89788d93c7dc28942c1d1028c087f4c319f113f9
Radu Rendec <radu.rendec@mindbit.ro>
2011-07-16 15:08:18 +0300

    Fix scale bug in BC class
    Signed-off-by: Radu Rendec <radu.rendec@mindbit.ro>

--- BC.php
+++ BC.php
@@ -1,4 +1,29 @@
 <?
+/*
+ * Mindbit PHP Library
+ * Copyright (C) 2009 Mindbit SRL
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of version 2.1 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/* It is extremely important to explicitly specify the scale for all bc*
+   functions. Otherwise, they will use the existing global setting (that
+   can be changed using bcscale()). For instance, in an Apache httpd
+   prefork mpm, there is no guarantee about the initial value seen by
+   php. In fact, it may inherit the default value set by previous
+   requests served by the same process.
+ */
 
 class BC {
 	static function baseConvert($number, $iBase, $oBase) {
@@ -11,17 +36,17 @@ class BC {
 				$c = $number[$i];
 				if ($c >= 'A')
 					$c = (string)(ord($c) - 55);
-				$dec = bcadd($dec, bcmul($pow, $c));
-				$pow = bcmul($pow, $iBase);
+				$dec = bcadd($dec, bcmul($pow, $c, 0), 0);
+				$pow = bcmul($pow, $iBase, 0);
 			}
 			$number = $dec;
 		}
 		if ($oBase == 10)
 			return $number;
 		$ret = '';
-		while (bccomp($number, "0") > 0) {
+		while (bccomp($number, "0", 0) > 0) {
 			$mod = bcmod($number, $oBase);
-			$number = bcdiv(bcsub($number, $mod), $oBase);
+			$number = bcdiv(bcsub($number, $mod, 0), $oBase, 0);
 			if ((int)$mod >= 10)
 				$mod = chr(55 + $mod);
 			$ret = $mod . $ret;
