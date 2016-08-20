61b4ffff182aca1f4fd1c40d7ea9605f35485034
Daniela Rendec <daniela.rendec@mindbit.ro>
2016-08-20 20:12:59 +0200

    Full PSR-2 and PSR-4 compliance
    Note: classes have not been tested for functionality.
    
    Signed-off-by: Daniela Rendec <daniela.rendec@mindbit.ro>

--- src/SmartClient/ConcurrentRestRequest.php
+++ src/SmartClient/ConcurrentRestRequest.php
@@ -1,20 +1,20 @@
 <?php
-/*    Mindbit PHP Library
- *    Copyright (C) 2009 Mindbit SRL
+/*
+ * Mindbit PHP Library
+ * Copyright (C) 2009 Mindbit SRL
  *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation; either
- *    version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of version 2.1 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
  *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 namespace Mindbit\Mpl\SmartClient;
