5707849a97131e9dd07a31fb234dd2dd9e28e1bc
Radu Rendec <radu.rendec@mindbit.ro>
2018-11-17 22:03:55 -0500

    Add helper class to build select option arrays
    Signed-off-by: Radu Rendec <radu.rendec@mindbit.ro>

---
+++ src/Mvc/View/Options.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Mindbit PHP Library
+ * Copyright (C) 2018 Mindbit SRL
+ *
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of version 2.1 of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+namespace Mindbit\Mpl\Mvc\View;
+
+class Options
+{
+    public static function buildFromSimpleMap($map, $selected)
+    {
+        $ret = [];
+        foreach ($map as $value => $text) {
+            $option = ['text' => $text];
+            if ($value == $selected) {
+                $option['selected'] = true;
+            }
+            $ret[$value] = $option;
+        }
+
+        return $ret;
+    }
+}
